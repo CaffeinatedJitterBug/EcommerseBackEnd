0a9bb47df37c50a2dc3fd22a2a05bd934c0fec03
CaffeinatedJitterBug <itwallaby@gmail.com>
2023-07-06 17:59:10 -0400

    Finished writing models and server.js and made some progress on the routes

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -4,25 +4,47 @@ const { Category, Product } = require('../../models');
 // The `/api/categories` endpoint
 
 router.get('/', (req, res) => {
-  // find all categories
-  // be sure to include its associated Products
+  Category.findAll().then((categoryData) => {
+    /* for (x=0; x<Category.id.length; x++) {
+
+    } */
+  });
 });
 
 router.get('/:id', (req, res) => {
-  // find one category by its `id` value
-  // be sure to include its associated Products
+  Category.findByPk(req.params.id).then((categoryData) => {
+
+  })
 });
 
 router.post('/', (req, res) => {
-  // create a new category
+  Category.create(req.body)
+    .then((category) => {
+      
+    })
 });
 
 router.put('/:id', (req, res) => {
-  // update a category by its `id` value
+  Category.update(req.body, {
+    where: {
+      id: req.params.id
+    }
+  })
+    .then((category) => {
+      
+    })
 });
 
 router.delete('/:id', (req, res) => {
-  // delete a category by its `id` value
+  Category.destroy({
+    where: {
+      id: req.params.id
+    }
+  })
+  .then((deletedCategory) => {
+    res.json(deletedCategory);
+  })
+  .catch((err) => res.json(err));
 });
 
 module.exports = router;
--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -4,25 +4,61 @@ const { Tag, Product, ProductTag } = require('../../models');
 // The `/api/tags` endpoint
 
 router.get('/', (req, res) => {
-  // find all tags
-  // be sure to include its associated Product data
+  Tag.findAll().then((TagData) => {
+    res.json(TagData);
+  });
 });
 
 router.get('/:id', (req, res) => {
-  // find a single tag by its `id`
-  // be sure to include its associated Product data
+  Tag.findByPk(req.params.id).then((TagData) => {
+    res.json(TagData);
+  });
 });
 
 router.post('/', (req, res) => {
-  // create a new tag
+  Tag.create(req.body)
+    .then((tag) => {
+      if (req.body.tagName) {
+        const TagArr = req.body.tagName.map((tag_name) => {
+          return {
+            tag_id,
+            tag_name: tag.tagName
+          };
+        });
+        return Tag.bulkCreate(TagArr);
+      }
+    })
+    .then((tagNames) => res.status(200).json(tagNames))
+    .catch((err) => {
+      console.log(err);
+      res.status(400).json(err);
+    });
 });
 
 router.put('/:id', (req, res) => {
-  // update a tag's name by its `id` value
+  Tag.update(req.body, {
+    where: {
+      id: req.params.id
+    },
+  })
+    .then((tag) => {
+      if (req.body.name) {
+        const mapName = tag.map(({  }) => )
+      }
+    })
 });
+  
 
 router.delete('/:id', (req, res) => {
-  // delete on tag by its `id` value
+  Tag.destroy({
+    where: {
+      id: req.params.id
+    }
+  })
+  .then((deletedTag) => {
+    res.json(deletedTag);
+  })
+  .catch((err) => res.json(err));
 });
 
 module.exports = router;
